83f70711223063cd4d955b20bb7e13e2c4f36ac5
A-Lyaska <2710747@gmail.com>
2023-03-16 13:05:14 +0300

    Nice import

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { initDB } from './db';
+import { ToDo } from './db/models/ToDo.model';
 
 @Controller()
 export class AppController {
@@ -9,4 +11,5 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @initDB();
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { initDB } from './db';
-import ToDo from './db/models/ToDo.model';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
